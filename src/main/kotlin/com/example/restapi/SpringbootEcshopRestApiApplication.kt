f0d6cbefa4223f06f4f5558d75bde01a5a70e9cf
RYA234 <mryuabc@gmail.com>
2022-10-21 11:55:05 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/restapi/SpringbootEcshopRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootEcshopRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootEcshopRestApiApplication>(*args)
+}
