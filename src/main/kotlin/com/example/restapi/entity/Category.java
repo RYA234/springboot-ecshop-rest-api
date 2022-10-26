af0c038fdd746f06e984acf9c4f029c389639c11
RYA234 <mryuabc@gmail.com>
2022-10-26 10:20:34 +0900

    Change Classes of parent children from Category to Integer.

--- src/main/kotlin/com/example/restapi/entity/Category.java
+++ src/main/kotlin/com/example/restapi/entity/Category.java
@@ -1,10 +1,14 @@
 package com.example.restapi.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
 @Table(name = "categories")
 public class Category{
     @Id
@@ -18,85 +22,8 @@ public class Category{
     @Column(length = 64, nullable = false, unique = true)
     private String alias;
 
-    @OneToMany(mappedBy = "parent")
-    @OrderBy("name asc")
-    private Set<Category> children = new HashSet<>();
-
-    @OneToOne
-    @JoinColumn(name ="parent_id")
-    private Category parent;
-
-    public Category() {
-    }
-
-    public Category(String name) {
-        this.name = name;
-        this.alias = name;
-    }
-
-    public Category(Integer id) {
-        this.id = id;
-    }
-
-    public Category(String name, Category parent) {
-        this(name);
-        this.parent = parent;
-    }
+    @Column(name ="parent_id")
+    private Integer parent;
 
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public Category getParent() {
-        return parent;
-    }
-
-    public Set<Category> getChildren() {
-        return children;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public void setChildren(Set<Category> children) {
-        this.children = children;
-    }
-
-    public void setParent(Category parent) {
-        this.parent = parent;
-    }
-
-    public static Category copyFull(Category category){
-        Category copyCategory = new Category();
-        copyCategory.setId((category.getId()));
-        copyCategory.setName((category.getName()));
-        copyCategory.setAlias((category.getAlias()));
-
-        return copyCategory;
-    }
-
-    public static Category copyFull(Category category,String name){
-        Category copyCategory = Category.copyFull(category);
-        copyCategory.setName(name);
-        return copyCategory;
-    }
 }
