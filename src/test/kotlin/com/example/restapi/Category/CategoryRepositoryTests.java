2abe2f845c461642847dffdc1c398f1fced054fc
RYA234 <mryuabc@gmail.com>
2022-10-28 11:01:43 +0900

    change name of grandParent folder

--- src/test/kotlin/com/example/restapi/Category/CategoryRepositoryTests.java
+++ src/test/kotlin/com/example/restapi/Category/CategoryRepositoryTests.java
@@ -1,15 +1,11 @@
 package com.example.restapi.Category;
 
-import com.example.restapi.category.CategoryRepository;
-import com.example.restapi.entity.Category;
+import com.example.restapi.implement.category.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
-
-import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
