fbf241b414d25dacb8146f8a57e6f4bc9b0ccd60
RYA234 <mryuabc@gmail.com>
2022-10-28 11:01:26 +0900

    change name of grandParent folder

--- src/main/kotlin/com/example/restapi/implement/category/CategoryRestController.java
+++ src/main/kotlin/com/example/restapi/implement/category/CategoryRestController.java
@@ -1,6 +1,5 @@
-package com.example.restapi.category;
+package com.example.restapi.implement.category;
 
-import com.example.restapi.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
