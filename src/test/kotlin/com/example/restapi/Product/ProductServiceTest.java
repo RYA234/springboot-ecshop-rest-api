6f713d750d7937770062886ef8fd6ae75b1841a1
RYA234 <mryuabc@gmail.com>
2022-11-09 14:15:48 +0900

    methodName and expected is changed getProductsByCategory_pageSize2_category1_first_page_Test()

--- src/test/kotlin/com/example/restapi/Product/ProductServiceTest.java
+++ src/test/kotlin/com/example/restapi/Product/ProductServiceTest.java
@@ -5,6 +5,7 @@ import com.example.restapi.domain.product.ProductRepository;
 import com.example.restapi.implement.product.ProductDto;
 import com.example.restapi.implement.product.ProductResponse;
 import com.example.restapi.implement.product.ProductServiceImplement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,19 +32,42 @@ public class ProductServiceTest {
     @InjectMocks
     private ProductServiceImplement productServiceImplement;
 
-    @Test
-    @DisplayName("000.getProductByCategoryのPagenationの変数（PageNoとPageSize）をテスト")
-    public void getProductByCategoryCheck(){
-        int pageNo = 0;
-        int pageSize = 4;
-        int categoryId =1;
 
+    List<Product> productList = new ArrayList<>();
+
+    @BeforeEach
+    public void prepareAlldata(){
+        // create all data by ProductDto
         // create List<Product>
-        List<Product> productList = new ArrayList<>();
+        // category_id
+        // 1= Fish
+        // 2= Meat
+        // 3= vegetable
+      //  List<Product> productList = new ArrayList<>();
         productList.add(new Product(1,"Sardin","This is a Sardin",false, 1,200,0.01f,0,"aa"));
         productList.add(new Product(2,"Maguro","This is a Maguro",false, 1,200,0.01f,0,"maguro_image"));
         productList.add(new Product(3,"Salmon","This is a Salmon",false, 1,200,0.01f,0,"maguro_image"));
-        productList.add(new Product(4,"Tuna","This is a Sardin",false, 1,200,0.01f,0,"maguro_image"));
+        productList.add(new Product(4,"Tuna","This is a Tuna",false, 1,200,0.01f,0,"maguro_image"));
+        productList.add(new Product(5,"Pork","This is a Pork",false, 2,330,0.01f,0,"porkImage"));
+        productList.add(new Product(6,"Beef","This is a Beef",false, 2,400,0.01f,0,"Beef_image"));
+        productList.add(new Product(7,"Chicken","This is a chicken",false, 2,800,0.01f,0,"Chicken_image"));
+        productList.add(new Product(8,"Spam","This is a Spam",false, 2,200,0.01f,0,"Spam_image"));
+        productList.add(new Product(9,"Cabbage","This is a Cabbage",false, 3,100,0.01f,0,"Cabbage_image"));
+        productList.add(new Product(10,"Carrot","This is a Carrot",false, 3,100,0.01f,0,"Carrot_image"));
+        productList.add(new Product(11,"EggPlant","This is a EggPlant",false, 3,100,0.01f,0,"EggPlant_image"));
+        productList.add(new Product(12,"Potato","This is a Potato",false, 3,100,0.01f,0,"Potato_image"));
+        productList.add(new Product(13,"Bean","This is a Bean",false, 3,100,0.01f,0,"Bean_image"));
+        productList.add(new Product(14,"Tomato","This is a Tomato",false, 3,100,0.01f,0,"Tomato_image"));
+        productList.add(new Product(15,"Nuts","This is a Nuts",false, 3,100,0.01f,0,"Nuts_image"));
+
+    }
+
+    @Test
+    @DisplayName("000.getProductByCategoryのテスト：ページサイズ2，categoryId=1(肉類)の最初ページが表示されるか")
+    public void getProductsByCategory_pageSize2_category1_first_page_Test(){
+        int pageNo = 0;
+        int pageSize = 2;
+        int categoryId =1;
 
         // convert List<Product> to Page<Product>
         Pageable pageable = PageRequest.of(pageNo, pageSize);
@@ -54,11 +78,20 @@ public class ProductServiceTest {
         // do service
         Mockito.doReturn(productPage).when(productRepository).getProductsByCategory(categoryId,pageable);
         ProductResponse actualResponse =  productServiceImplement.getProductsByCategory(pageNo,pageSize,categoryId);
-
+        // todo why categoryId is decrease?
 
         // create expected Data
-        List<ProductDto> content = productList.stream().map(product -> productServiceImplement.mapToDTO(product)).collect(Collectors.toList());
-        ProductResponse expectedReponse = new ProductResponse(content,pageNo,pageSize,4,1,true,0,null);
+        List<Product> expectedProductList = new ArrayList<>();
+        expectedProductList.add(new Product(1,"Sardin","This is a Sardin",false, 1,200,0.01f,0,"aa"));
+        expectedProductList.add(new Product(2,"Maguro","This is a Maguro",false, 1,200,0.01f,0,"maguro_image"));
+        expectedProductList.add(new Product(3,"Salmon","This is a Salmon",false, 1,200,0.01f,0,"maguro_image"));
+        expectedProductList.add(new Product(4,"Tuna","This is a Tuna",false, 1,200,0.01f,0,"maguro_image"));
+        List<ProductDto> content = expectedProductList.stream().map(product -> productServiceImplement.mapToDTO(product)).collect(Collectors.toList());
+
+
+
+
+        ProductResponse expectedReponse = new ProductResponse(content,pageNo,pageSize,4,2,false,1,null);
         //ProductResponse(List< ProductDto > content, int pageNo, int pageSize, long totalElements, int totalPages, boolean last, int categoryId, String categoryName)
 
         // compare actual and expected
