d99b678cb467660f98b30f117e95f78a5f0154c0
RYA234 <mryuabc@gmail.com>
2022-11-22 13:13:54 +0900

    Initial commit

--- src/main/kotlin/com/example/restapi/implement/customer/CustomerServiceImplement.java
+++ src/main/kotlin/com/example/restapi/implement/customer/CustomerServiceImplement.java
@@ -1,4 +1,49 @@
 package com.example.restapi.implement.customer;
 
-public class CustomerServiceImplement {
+import com.example.restapi.domain.customer.CustomerRepository;
+import com.example.restapi.domain.customer.CustomerService;
+import com.example.restapi.implement.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class CustomerServiceImplement implements CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+    @Override
+    public LoginDto findByEmail() {
+        return null;
+    }
+
+    //todo postmanでidが取得できるところまで確認できた。idを使ってcartの中身を確認できるようにする
+
+    public Integer getIdfromJwtToken(HttpServletRequest request){
+        // get Jwt from Response
+        String jwtToken = getJWTformRequest(request);
+        // get email from jwt
+        String email = jwtTokenProvider.getCustomernameFromJWT(jwtToken);
+
+        return customerRepository.findByEmail(email).getId();
+    }
+
+
+    private String getJWTformRequest(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+
+
+
+
 }
