fc68770119058af10611c10fdc3782009fc6143f
RYA234 <mryuabc@gmail.com>
2022-11-22 13:15:52 +0900

    Initial commit getIdFromJwtToken is added because of implementing CartItemService

--- src/main/kotlin/com/example/restapi/domain/customer/CustomerService.java
+++ src/main/kotlin/com/example/restapi/domain/customer/CustomerService.java
@@ -1,5 +1,24 @@
 package com.example.restapi.domain.customer;
 
+import com.example.restapi.implement.customer.LoginDto;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+// todo extract register and login method from controller
+@Service
 public interface CustomerService {
+        public LoginDto findByEmail();
+
+        // registerUser
+
+        //
+
+        // loginUser
+
+        // JwtトークンからCustomerのidを取得する関数
+        // ユーケース上ではCartitemとOrderで使われる
+        public Integer getIdfromJwtToken(HttpServletRequest request);
 
 }
