a2ae9674c35909662b95db6def2b6b19f2948cf9
RYA234 <mryuabc@gmail.com>
2022-11-22 13:24:09 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/restapi/implement/customer/CustomerDto.java
@@ -0,0 +1,16 @@
+package com.example.restapi.implement.customer;
+
+// todo まだ使われていない
+public class CustomerDto {
+    private Integer id;
+    private String password;
+    private String email;
+    private String JwtToken;
+
+
+    private String getIdFromResponse(){
+        return "a";
+    }
+
+
+}
