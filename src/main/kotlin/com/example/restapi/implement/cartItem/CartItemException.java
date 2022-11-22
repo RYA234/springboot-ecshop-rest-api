5185fbe9438f88c08b5673093de0d8cbe62529d2
RYA234 <mryuabc@gmail.com>
2022-11-22 13:30:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/restapi/implement/cartItem/CartItemException.java
@@ -0,0 +1,7 @@
+package com.example.restapi.implement.cartItem;
+
+public class CartItemException extends Exception {
+    public CartItemException(String message){
+        super(message);
+    }
+}
