550e9ae2752521e554923d61d24ca64a51072a6e
RYA234 <mryuabc@gmail.com>
2022-11-28 10:41:59 +0900

    TestCode of findByCustomerId is added.

--- src/test/kotlin/com/example/restapi/order/OrderRepositoryTest.java
+++ src/test/kotlin/com/example/restapi/order/OrderRepositoryTest.java
@@ -7,7 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -22,7 +31,7 @@ public class OrderRepositoryTest {
     @DisplayName("Orderを引数とし、Saveを実行したとき、Orderが返される。")
     public void givenOrder_whenSave_thenReturnOrder() {
         // given-precondition or Setup
-        Order newOrder = new Order();
+        Order newOrder = new Order(0, toDate(LocalDateTime.now()),6000f,100f,6100f,610f,6710f,"pending");
         //when - action or the behavior that we are going test
         Order savedOrder = orderRepository.save(newOrder);
         //then - verify the output
@@ -33,10 +42,19 @@ public class OrderRepositoryTest {
     @DisplayName("cutomerIdとPageableを引数とし、findByCustomerIdを実行したとき、ページネーションかされたOrdersが返される。")
     public void givenCustomerIdandPageable_whenFindByCustomerId_thenReturnPageOrder() {
         // given-precondition or Setup
-
+        Integer customerId = 0;
+        // make pageable
+        int pageNo = 0;
+        int pageSize =2;
+        Pageable pageable = PageRequest.of(pageNo,pageSize);
         //when - action or the behavior that we are going test
-
+        Page<Order> actualOrder = orderRepository.findByCustomerId(customerId,pageable);
         //then - verify the output
+        int expectedTotal =3;
+        int expectedContentSize = 2;
+        assertThat(actualOrder.getTotalElements()).isEqualTo(expectedTotal);
+        assertThat(actualOrder.getContent().size()).isEqualTo(expectedContentSize);
+
     }
 
     @Test
@@ -48,5 +66,12 @@ public class OrderRepositoryTest {
         Order actualOrder = new Order();
         //then - verify the output
         Order expectedOrder = orderRepository.findOrderById(id);
+    }
+
+    private Date toDate(LocalDateTime localDateTime) {
+        ZoneId zone = ZoneId.systemDefault();
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zone);
+        Instant instant = zonedDateTime.toInstant();
+        return Date.from(instant);
     }
 }
