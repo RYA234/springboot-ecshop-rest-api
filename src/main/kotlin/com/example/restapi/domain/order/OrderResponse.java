3881b32fd3b79249d2012b720aada906fe223aea
RYA234 <mryuabc@gmail.com>
2022-12-06 12:55:48 +0900

    javadoc is added

--- src/main/kotlin/com/example/restapi/domain/order/OrderResponse.java
+++ src/main/kotlin/com/example/restapi/domain/order/OrderResponse.java
@@ -5,7 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+/**
+ *
+ * @brief:  Order GET Responese
+ *
+ * @description  顧客がいままで注文した履歴をすべて取得するときに使われるレスポンスボディです。
+ *               Page<Order>から変換される形でService層で使われます。
+ *               Page<Order>から変換する理由としては、restapi側からするとPage<Order>がわかりにくいためです。
+ *
+ *
+ * @Auther RYA234
+ *
+ * @See: {＠link  OrderRestController}
+ */
 @Data
 @NoArgsConstructor
 public class OrderResponse {
