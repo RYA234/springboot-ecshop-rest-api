9890d87d6bf5c72d8f902170c259b04d5a595345
RYA234 <mryuabc@gmail.com>
2022-12-06 12:57:13 +0900

    add constructor. it is used in OrderServiceImplementTest

--- src/main/kotlin/com/example/restapi/implement/order/OrderDto.java
+++ src/main/kotlin/com/example/restapi/implement/order/OrderDto.java
@@ -4,6 +4,17 @@ import lombok.Data;
 
 import java.util.Date;
 
+/**
+ * @brief: This Class is Data To Object in Order.
+ *
+ * @description　when Service method is done,Order Class convert to OrderDto Class.
+ *                    OrderDto can do change OrderClass not changing OrderEntity and write test code easily.
+ *
+ * @Auther RYA234
+ *
+ * @Entity: {@link  com.example.restapi.domain.order.Order}
+ * @UseCase: {@link com.example.restapi.domain.order.OrderService}
+ */
 @Data
 public class OrderDto {
     private Integer id;
@@ -15,4 +26,20 @@ public class OrderDto {
     private float tax;
     private float total;
     private String status;
+
+    public OrderDto(Integer id, Integer customerId, Date orderTime, float productCost, float shippingCost, float subtotal, float tax, float total, String status) {
+        this.id = id;
+        this.customerId = customerId;
+        this.orderTime = orderTime;
+        this.productCost = productCost;
+        this.shippingCost = shippingCost;
+        this.subtotal = subtotal;
+        this.tax = tax;
+        this.total = total;
+        this.status = status;
+    }
+
+    public OrderDto() {
+
+    }
 }
