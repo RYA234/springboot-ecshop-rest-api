ddea574ec80615653be1d77bf461a5ccd30df1da
RYA234 <mryuabc@gmail>
2022-12-22 08:38:29 +0900

    nextjs からでもアクセスできるように @CrossOriginを追加

--- src/main/kotlin/com/example/restapi/implement/customer/CustomerRestController.java
+++ src/main/kotlin/com/example/restapi/implement/customer/CustomerRestController.java
@@ -12,11 +12,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:3000")
 public class CustomerRestController {
 
     @Autowired
