c03970db9e7434446df3431edadf74683b78a113
RYA234 <mryuabc@gmail>
2023-01-26 10:27:31 +0900

    Customer　Repositoryレイヤー追加　テスト済み

--- src/main/kotlin/com/example/restapi/domain/customer/Customer.java
+++ src/main/kotlin/com/example/restapi/domain/customer/Customer.java
@@ -21,8 +21,8 @@ import javax.persistence.*;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
     private Integer id;
 
     // Loginするときのパスワードして使う
@@ -32,4 +32,22 @@ public class Customer {
     // ログインするときのIDとして使用する
     @Column(nullable = false, unique = true, length = 45)
     private String email;
+
+    // 認証コード
+    @Column(name = "verification_code", length = 64)
+    private String verificationCode;
+
+    // 初期値はfalse 認証が成功すると trueになる。
+    private boolean enabled;
+
+    public Customer(Integer id, String password, String email, String verificationCode, boolean enabled) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.verificationCode = verificationCode;
+        this.enabled = enabled;
+    }
+
+    public Customer() {
+    }
 }
